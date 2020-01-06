3888db998e3576625359e4bfab7c0e8ecbc09d2b
Donwaztok <igor_irm_13@hotmail.com>
2020-01-06 16:54:04 -0300

    Adicionado Rotas no Google Maps

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, PermissionsAndroid } from 'react-native';
+
 import { Pages } from 'react-native-pages';
 import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import getDirections from 'react-native-google-maps-directions';
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyASspOu0rSMaUOR28MNDZlkEhspkWnKkJo';
 
 export default class App extends Component {
 	state = {
 		mapRegion: null,
+		origin: { latitude: -23.5607, longitude: -46.6293 },
+		destination: { latitude: -23.5747, longitude: -46.6369 },
 		lastLat: null,
 		lastLong: null,
 		bottomMargin: 1,
@@ -26,42 +33,23 @@ export default class App extends Component {
 		}
 	}
 
-	getLocation = () => {
-		navigator.geolocation.getCurrentPosition((position) => {
-			let newOrigin = {
-				latitude: position.coords.latitude,
-				longitude: position.coords.longitude
-			};
-			this.setState({
-				origin: newOrigin
-			});
-		}, (err) => {
-			console.log('error');
-			console.log(err)
-		}, { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 })
-	};
-
 	async componentDidMount() {
 		let isGranted = await this.requestLocationPermission();
-		if (isGranted) {
-			this.getLocation();
-		}
-		this.getLocation();
 	}
 
 	handleGetGoogleMapDirections = () => {
-        const data = {
-            source: this.state.origin,
-            destination: this.state.destination,
-            params: [
-                {
-                  key: "travelmode",
-                  value: "driving"
-                }
-            ]
-        };
-        getDirections(data)
-      };
+		const data = {
+			source: this.state.origin,
+			destination: this.state.destination,
+			params: [
+				{
+					key: "travelmode",
+					value: "driving"
+				}
+			]
+		};
+		getDirections(data)
+	};
 
 	render() {
 		return (
@@ -70,28 +58,44 @@ export default class App extends Component {
 				<View style={{ flex: 1, backgroundColor: 'green' }} />
 				<View style={{ flex: 1, backgroundColor: 'blue' }}>
 					<MapView
+						ref={map => this.mapView = map}
 						style={[styles.map, { marginBottom: this.state.bottomMargin }]}
-						region={this.state.mapRegion}
 						showsUserLocation={true}
 						followsUserLocation={true}
 						showsCompass={true}
+						loadingEnabled={true}
+						toolbarEnabled={true}
 						zoomControlEnabled={true}
 						showsScale={true}
 						showsBuildings={true}
-						showsTraffic={true}
+						//showsTraffic={true}
 						showsIndoors={true}
-						onMapReady={() => this.setState({ bottomMargin: 0 })}>
-						<MapView.Marker
-							coordinate={{
-								latitude: (this.state.lastLat + 1.00050) || -36.82339,
-								longitude: (this.state.lastLong + 1.00050) || -73.03569,
-							}}>
-							<View>
-								<Text style={{ color: '#000' }}>
-									{this.state.lastLong} / {this.state.lastLat}
-								</Text>
-							</View>
+						onMapReady={() => this.setState({ bottomMargin: 0 })}
+						region={{
+							latitude: (this.state.origin.latitude + this.state.destination.latitude) / 2,
+							longitude: (this.state.origin.longitude + this.state.destination.longitude) / 2,
+							latitudeDelta: Math.abs(this.state.origin.latitude - this.state.destination.latitude) + Math.abs(this.state.origin.latitude - this.state.destination.latitude) * .5,
+							longitudeDelta: Math.abs(this.state.origin.longitude - this.state.destination.longitude) + Math.abs(this.state.origin.longitude - this.state.destination.longitude) * .5,
+						}}
+					>
+						<MapView.Marker coordinate={this.state.destination} >
+							<MapView.Callout onPress={this.handleGetGoogleMapDirections}>
+								<Text>Destino (Clique para abrir no Google Maps)</Text>
+							</MapView.Callout>
 						</MapView.Marker>
+
+						<MapView.Marker coordinate={this.state.origin} >
+							<MapView.Callout>
+								<Text>Inicio</Text>
+							</MapView.Callout>
+						</MapView.Marker>
+
+						<MapViewDirections
+							strokeWidth={4}
+							origin={this.state.origin}
+							destination={this.state.destination}
+							apikey={GOOGLE_MAPS_APIKEY}
+						/>
 					</MapView>
 				</View>
 			</Pages>
